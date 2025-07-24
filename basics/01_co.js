2c85fc8798bf4494da7bbb2e5d5494387c5f71b6
billy x <billyx719@gmail.com>
2025-07-24 07:21:18 +0000

    container

--- basics/01_co.js
+++ basics/01_co.js
@@ -1,13 +1,24 @@
-let score = true
+//let score = true
 
 //console.log(typeof score);
 //console.log(typeof (score));//
 
-let valueinnubmer=Number(score)
+//let valueinnubmer=Number(score)
 //console.log(valueinnubmer);
 //console.log(typeof valueinnumber);//
 
-let somenumber = 44
-let stringnumber= String(somenumber);
-console.log(stringnumber);
-console.log(typeof stringnumber);
+//let somenumber = 44
+//let stringnumber= String(somenumber);
+//console.log(stringnumber);
+//console.log(typeof string);//
+//opoeartors//
+
+let x = 5
+let a = ++x
+console.log(a)
+console.log(x)
+
+let y = 5
+let b = x++
+console.log(y)
+console.log(b)
