ecfbaaec3e21a3d0b36a40768ea10b9b9d8d19fe
billy x <billyx719@gmail.com>
2025-07-25 06:35:32 +0000

    container

---
+++ basics/sh.js
@@ -0,0 +1,20 @@
+// let myname = "sherry"
+// let anothername = myname
+
+// anothername = "umer"
+
+// console.log(myname);
+// console.log(anothername);
+
+
+
+let userOne=
+ {
+    email : "sherry@gmail.com"
+}
+
+let userTwo=userOne
+userTwo.email="umer@gmail.com"
+
+console.log(userOne.email)
+console.log(userTwo.email)
